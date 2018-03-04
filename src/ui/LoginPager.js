956654097f18f266a89b45d82e8dfc2fafe49805
shanda4869@gmail.com <shanda4869@gmail.com>
2018-03-04 19:46:02 +0800

    android commit

--- src/ui/LoginPager.js
+++ src/ui/LoginPager.js
@@ -10,11 +10,11 @@ import {
     StyleSheet,
     Text,
     View,
-    Image,
+    Platform,
     TextInput,
     TouchableOpacity,
     KeyboardAvoidingView,
-    ScrollView, Switch, Alert,AsyncStorage
+    ScrollView, Switch, Alert, AsyncStorage
 } from 'react-native';
 import {MenuProvider} from 'react-native-popup-menu';
 import Loading from 'react-native-loading-spinner-overlay';
@@ -33,7 +33,8 @@ import {SceneMap, TabBar, TabViewAnimated} from "react-native-tab-view";
 import ApiService from '../api/ApiService';
 import Utils from '../Utils';
 import ScrollableTabView, {DefaultTabBar,} from 'react-native-scrollable-tab-view';
-
+import AndroidModule from '../module/AndoridCommontModule'
+import IosModule from '../module/IosCommontModule'
 const Dimensions = require('Dimensions');
 const {width, height} = Dimensions.get('window');
 export default class LoginPager extends Component<{}> {
@@ -42,7 +43,7 @@ export default class LoginPager extends Component<{}> {
         this.state = {
             isLoading: false,
             phone: '18680006907',
-            codeCheck:'1',
+            codeCheck: '',
             check: false,
             index: 0,
 
@@ -56,6 +57,8 @@ export default class LoginPager extends Component<{}> {
 
             //  providerCount:0
             textTemp: '123',
+
+            smsSpeed: 60
         };
     }
 
@@ -78,6 +81,20 @@ export default class LoginPager extends Component<{}> {
             }
         });
         return str.substring(0, str.length - 1);
+    }
+
+    countDown() {
+        let t1 = setInterval(() => {
+            this.state.smsSpeed--;
+            if (this.state.smsSpeed < 1) {
+                this.state.smsSpeed = 60;
+                this.setState({});
+
+                clearInterval(t1);
+            } else {
+                this.setState({})
+            }
+        }, 1000);
     }
 
     register() {
@@ -125,15 +142,46 @@ export default class LoginPager extends Component<{}> {
 
     }
 
-    login() {
-        if (!this.state.phone ||!this.state.codeCheck) {
+    smsSend() {
+        if (!this.state.phone) {
+            Toast.show("请填写手机号码");
+            return
+        }
+        this.countDown();
+        if (Platform.OS === 'android') {
+            AndroidModule.sendCode("86", this.state.phone, (result) => {
+                console.log(result);
+                Toast.show(result === -1 ? "发送成功" : ("发送验证码失败：" + result))
+            })
+        } else {
+
+        }
+    }
+
+    smsLogin() {
+        if (!this.state.phone || !this.state.codeCheck) {
             Toast.show("信息不完整");
             return
         }
         this.setState({isLoading: true});
 
-        //msg code check then login
+        if (Platform.OS === 'android') {
+            AndroidModule.submitCode("86", this.state.phone, this.state.codeCheck, (result) => {
+                console.log(result);
+                if (result === -1) {
+                    this.login()
+                } else {
+                    this.setState({isLoading: false});
+                    Toast.show("验证失败：" + result)
+                }
+            })
+        } else {
 
+        }
+
+    }
+// java.lang.RuntimeException: Illegal callback invocation from native module. This callback type only permits a single invocation from native code.
+    login() {
         ApiService.login(this.state.phone)
             .then((responseJson) => {
                 this.setState({isLoading: false,});
@@ -159,9 +207,8 @@ export default class LoginPager extends Component<{}> {
                 this.setState({isLoading: false,});
                 Toast.show("出错了，请稍后再试");
             }).done();
+
     }
-
-
 
 
     topView() {
@@ -185,23 +232,29 @@ export default class LoginPager extends Component<{}> {
                 style={{margin: 16}}
                 label={"手机号码"}
                 borderColor={Color.colorBlue}
+                keyboardType={"numeric"}
+                value={this.state.phone}
                 onChangeText={(text) => this.setState({phone: text})}/>
             <View style={{flexDirection: 'row'}}>
                 <Hoshi
                     style={{marginLeft: 16, marginRight: 16, width: width / 2}}
                     label={'验证码'}
+                    keyboardType={"numeric"}
                     borderColor={Color.colorBlue}
+                    value={this.state.codeCheck}
                     onChangeText={(text) => this.setState({codeCheck: text})}/>
                 <TouchableOpacity
+                    disabled={this.state.smsSpeed !== 60}
                     style={{width: width / 2, alignItems: 'center', justifyContent: 'center'}}
-                    onPress={() => {
-                    }}>
-                    <Text style={{color: Color.colorBlue}}>获取验证码</Text>
+                    onPress={() => this.smsSend()}>
+                    <Text style={{color: this.state.smsSpeed === 60 ? Color.colorBlue : Color.content}}>
+                        { this.state.smsSpeed === 60 ? '获取验证码' : this.state.smsSpeed + "秒"}
+                    </Text>
                 </TouchableOpacity>
             </View>
             <TouchableOpacity
                 style={[styles.button, {backgroundColor: Color.colorBlue,}]}
-                onPress={() => this.login() }>
+                onPress={() => this.smsLogin()}>
                 <Text style={{color: 'white'}}>登录</Text>
             </TouchableOpacity>
         </View>
@@ -213,12 +266,15 @@ export default class LoginPager extends Component<{}> {
             <Hoshi
                 style={{margin: 16}}
                 label={'用户名'}
+                value={this.state.registerUser}
                 borderColor={Color.colorBlue}
                 onChangeText={(text) => this.setState({registerUser: text})}
             />
             <Hoshi
                 style={{marginLeft: 16, marginRight: 16}}
                 label={'电话号码'}
+                keyboardType={"numeric"}
+                value={this.state.registerPhone}
                 borderColor={Color.colorBlue}
                 onChangeText={(text) => this.setState({registerPhone: text})}
             />
